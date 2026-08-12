66b0b0494e924b589c3a8050ddedd6374ea842a4
zippatias <119546362+zippatias@users.noreply.github.com>
2026-08-13 01:32:51 +0200

    Prima versione

---
+++ echo-khinsider-extension/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+val extName: String by settings
+rootProject.name = extName
+include(":app")
+include(":ext")
